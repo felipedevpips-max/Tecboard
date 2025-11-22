3af9c3e96a1f3562b866471a9aabd8654449d1e0
felipedevpips-max <felipedevpips@gmail.com>
2025-11-22 12:05:43 -0300

    primeiro componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
 import "./App.css";
+//NO REACT,COMPONENTES SÃO FUNÇÕES
+
+function FormularioDeEvento() {
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">Qual o nome do evento?</label>
+        <input type="text" id="nome"></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="data">Data do evento</label>
+        <input type="date" id="data"></input>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="tema">Tema do evento</label>
+        <input type="text" id="tema"></input>
+      </fieldset>
+    </form>
+  );
+}
 
 function App() {
   return (
@@ -9,6 +30,7 @@ function App() {
       <section>
         <img src="/public/banner.png" alt="" />
       </section>
+      <FormularioDeEvento />
     </main>
   );
 }
