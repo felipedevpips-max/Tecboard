89697de86c5d9ac91fa1eb0f840c4d3b1c3964ee
felipedevpips-max <felipedevpips@gmail.com>
2025-11-22 10:59:11 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="" alt="" />
+      </header>
+      <section>
+        <img src="" alt="" />
+      </section>
+    </main>
+  );
+}
+
+export default App;
